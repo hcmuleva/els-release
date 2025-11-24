1dda1a27adb3d1c42a192675bc3ae6845f681871
hcmuleva <hcmuleva@gmail.com>
2025-11-25 00:27:10 +0530

    fix: simplify env.sh script for better compatibility

--- fullstack_level3/development/client/env.sh
+++ fullstack_level3/development/client/env.sh
@@ -9,6 +9,6 @@ echo "window._env_ = {" >> /usr/share/nginx/html/env-config.js
 
 # Read each line in .env file
 # Each line represents key=value pairs
-printenv | grep VITE_ | awk -F = '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' >> /usr/share/nginx/html/env-config.js
+printenv | grep VITE_ | awk -F = '{ print "  \"" $1 "\": \"" $2 "\"," }' >> /usr/share/nginx/html/env-config.js
 
 echo "}" >> /usr/share/nginx/html/env-config.js
